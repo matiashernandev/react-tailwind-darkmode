fd817477e3fb71abca59fdb1329fc3d75f57ac0c
nkwaaaa <nkwaaaa@gmail.com>
2023-09-14 08:49:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+
+    <div className="h-screen flex justify-center items-center text-bold text-4xl text-blue-500 bg-blue-950 text-center">
+      <button className="bg-slate-200 font-bold px-4 py-2 rounded hover:bg-slate-300">
+        Change Theme
+      </button>
+    </div>
+  )
+}
