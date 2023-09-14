63cbf99bb2ab5c662f87033ec7c9e0ce269313da
nkwaaaa <nkwaaaa@gmail.com>
2023-09-14 09:09:24 -0300

    add darkmode with cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-export default function App() {
-  return (
+import { useEffect } from "react";
+import { useState } from "react";
 
-    <div className="h-screen flex justify-center items-center text-bold text-4xl text-blue-500 bg-blue-950 text-center">
-      <button className="bg-slate-200 font-bold px-4 py-2 rounded hover:bg-slate-300">
+export default function App() {
+  const [theme, setTheme] = useState("light");
+  const handleThemeChange = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setTheme("light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  useEffect(() => {
+    const userPrefersDark =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (userPrefersDark) {
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
+
+  return (
+    <div className="h-screen flex justify-center items-center text-bold text-4xl dark:text-blue-700 dark:bg-blue-950 text-center">
+      <button
+        onClick={handleThemeChange}
+        className="dark:bg-slate-200 font-bold px-4 py-2 border-4 rounded dark:hover:bg-slate-300 "
+      >
         Change Theme
       </button>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // O 'media' si prefieres el modo oscuro basado en la configuración del sistema
   theme: {
     extend: {},
   },
